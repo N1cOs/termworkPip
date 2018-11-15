ae1d3b775b741277bf56cfb4e445a2d44af9a8d4
N1cOs <N1cOs@users.noreply.github.com>
2018-11-15 21:13:50 +0300

    added tests for repositories

--- src/main/java/ru/ifmo/se/termwork/config/JpaConfig.java
+++ src/main/java/ru/ifmo/se/termwork/config/JpaConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -40,5 +42,12 @@ public class JpaConfig {
         dataSource.setUsername(environment.getProperty("user"));
         dataSource.setPassword(environment.getProperty("password"));
         return dataSource;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager getTransactionManager(){
+        DataSourceTransactionManager manager = new DataSourceTransactionManager();
+        manager.setDataSource(getDataSource());
+        return manager;
     }
 }
--- src/test/java/JpaTests.java
+++ src/test/java/JpaTests.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.ifmo.se.termwork.config.JpaConfig;
+import ru.ifmo.se.termwork.domain.Student;
+import ru.ifmo.se.termwork.repository.StudentRepository;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {JpaConfig.class})
 public class JpaTests {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void testStudent(){
+
+        Student student = studentRepository.findById(1).get();
+        System.out.println(student.getName());
+
+    }
 }
