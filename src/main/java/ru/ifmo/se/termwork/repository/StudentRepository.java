a92c1fb970461eefdbb3f6e359a665d28207f6cd
N1cOs <N1cOs@users.noreply.github.com>
2018-11-19 23:14:46 +0300

    added delete exam

--- src/main/java/ru/ifmo/se/termwork/repository/StudentRepository.java
+++ src/main/java/ru/ifmo/se/termwork/repository/StudentRepository.java
@@ -8,10 +8,15 @@ import ru.ifmo.se.termwork.domain.Student;
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Modifying
     @Query(value = "insert into exam values (?1, ?2, ?3)", nativeQuery = true)
-    void saveExam(Integer studentId, Integer subjectId, Integer score);
+    void addExam(Integer studentId, Integer subjectId, Integer score);
 
     @Modifying
     @Query(value = "update exam set score = ?3 where id_student = ?1 and id_subj = ?2",
             nativeQuery = true)
     void updateExam(Integer studentId, Integer subjectId, Integer score);
+
+    @Modifying
+    @Query(value = "delete from exam where id_student = ?1 and id_sudj = ?2", nativeQuery = true)
+    void deleteExam(Integer studentId, Integer subjectId);
+
 }
