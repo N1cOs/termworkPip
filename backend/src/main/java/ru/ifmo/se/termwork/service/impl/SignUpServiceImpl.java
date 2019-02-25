2c415e1d6ef8129e137e89d2f8d960e442590409
dmitriyace <chistohodovda@gmail.com>
2019-02-25 16:52:18 +0300

    correct sql exceptions handling

--- backend/src/main/java/ru/ifmo/se/termwork/service/impl/SignUpServiceImpl.java
+++ backend/src/main/java/ru/ifmo/se/termwork/service/impl/SignUpServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.postgresql.util.PSQLException;
 import org.springframework.core.NestedExceptionUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -46,40 +47,14 @@ public class SignUpServiceImpl implements SignUpService {
         Map<Integer, InputError> inputErrors = new HashMap<>();
         inputErrors.put(StudentRepository.SqlError.EMAIL,
                 new InputError("email", "exception.signUp.email"));
-        inputErrors.put(StudentRepository.SqlError.EMAIl_PHONE,
-                new InputError("email, phone", "exception.signUp.email_phone"));
-        inputErrors.put(StudentRepository.SqlError.EMAIL_SERIAL_NUMBER,
-                new InputError("email, serial number", "exception.signUp.email_serialNumber"));
         inputErrors.put(StudentRepository.SqlError.SERIAL_NUMBER,
                 new InputError("serial number", "exception.signUp.serialNumber"));
         inputErrors.put(StudentRepository.SqlError.PHONE,
                 new InputError("phone", "exception.signUp.phone"));
-        inputErrors.put(StudentRepository.SqlError.SERIAL_NUMBER_PHONE,
-                new InputError("phone, serial number", "exception.signUp.phone_serialNumber"));
-        inputErrors.put(StudentRepository.SqlError.ALL,
-                new InputError("email, phone and serial number", "exception.signUp.all"));
-
         errors = Collections.unmodifiableMap(inputErrors);
     }
 
-    void chooseState(String sqlState) throws ApiException{
-        switch (sqlState){
-            case "23030":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.EMAIL).getInfo());
-            case "23031":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.SERIAL_NUMBER).getInfo());
-            case "23032":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.PHONE).getInfo());
-            case "23033":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.EMAIL).getInfo());
-            case "23034":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.EMAIL_SERIAL_NUMBER).getInfo());
-            case "23035":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.SERIAL_NUMBER_PHONE).getInfo());
-            case "23036":
-                throw new ApiException(HttpStatus.FORBIDDEN, errors.get(StudentRepository.SqlError.ALL).getInfo());
-        }
-    }
+
 
     @Override
     public void signUp(StudentDto studentDto) {
@@ -91,9 +66,9 @@ public class SignUpServiceImpl implements SignUpService {
         //ToDo: exception handling
         try {
             studentRepository.save(student);
-        } catch (Exception ex) {
+        } catch (DataIntegrityViolationException ex) {
             PSQLException psqlException = (PSQLException) NestedExceptionUtils.getMostSpecificCause(ex);
-            chooseState(psqlException.getSQLState());
+            throwApiExceptionUponState(psqlException.getSQLState());
         }
 
         saveToJabber(studentDto.getEmail(), studentDto.getPassword());
@@ -108,4 +83,34 @@ public class SignUpServiceImpl implements SignUpService {
     }
 
 
+    private void throwApiExceptionUponState(String sqlState) throws ApiException{
+        switch (Integer.parseInt(sqlState)) {
+            case StudentRepository.SqlError.EMAIL:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Collections.singletonList(errors.get(StudentRepository.SqlError.EMAIL)));
+            case StudentRepository.SqlError.SERIAL_NUMBER:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Collections.singletonList(errors.get(StudentRepository.SqlError.SERIAL_NUMBER)));
+            case StudentRepository.SqlError.PHONE:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Collections.singletonList(errors.get(StudentRepository.SqlError.PHONE)));
+            case StudentRepository.SqlError.EMAIl_PHONE:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Arrays.asList(errors.get(StudentRepository.SqlError.EMAIL),
+                                errors.get(StudentRepository.SqlError.PHONE)));
+            case StudentRepository.SqlError.EMAIL_SERIAL_NUMBER:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Arrays.asList(errors.get(StudentRepository.SqlError.EMAIL),
+                                errors.get(StudentRepository.SqlError.SERIAL_NUMBER)));
+            case StudentRepository.SqlError.SERIAL_NUMBER_PHONE:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Arrays.asList(errors.get(StudentRepository.SqlError.SERIAL_NUMBER),
+                                errors.get(StudentRepository.SqlError.PHONE)));
+            case StudentRepository.SqlError.ALL:
+                throw new ApiException(HttpStatus.BAD_REQUEST,
+                        Arrays.asList(errors.get(StudentRepository.SqlError.EMAIL),
+                                errors.get(StudentRepository.SqlError.SERIAL_NUMBER),
+                                errors.get(StudentRepository.SqlError.PHONE)));
+        }
+    }
 }
