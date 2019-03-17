aefaf98962863283900a93bd3d768fcb32c5a9bd
dmitriyace <chistohodovda@gmail.com>
2019-03-17 21:33:21 +0300

    added User type

---
+++ fe-vue/src/types/User.ts
@@ -0,0 +1,10 @@
+export default interface User {
+    surname: string
+    name: string
+    patronymic?: string
+    birthDate?: string
+    serialNumber: string
+    email: string
+    phone?: string
+    password: string
+}
