08bf0bec0887be34e57d0561dbc6500ab6f4f568
dmitriyace <chistohodovda@gmail.com>
2019-03-20 00:41:27 +0300

    views: add Registration view to use with element ui

---
+++ fe-vue/src/views/Registration.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="form">
+        <el-form>
+            <el-form-item>
+
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
