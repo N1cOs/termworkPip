d26e4bb51e81d476983c978f3dd5de28fcdcc2de
dmitriyace <chistohodovda@gmail.com>
2019-03-25 23:53:01 +0300

    views: add rating view

---
+++ fe-vue/src/views/Ratings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import RatingRecord from "@/types/RatingRecord";
+
+@Component
+export default class Ratings extends Vue {
+    ratings: RatingRecord[]
+}
+</script>
+
+<style scoped></style>
