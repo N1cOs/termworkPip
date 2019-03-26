58118cd0ed064f571c5b3df1d2027b7625ade298
dmitriyace <chistohodovda@gmail.com>
2019-03-27 01:23:58 +0300

    fe-vue: ratings: add initial ratings component

--- fe-vue/src/views/Ratings.vue
+++ fe-vue/src/views/Ratings.vue
@@ -1,15 +1,25 @@
 <template>
-  <div></div>
+
+  <el-container>
+      <el-header>
+
+      </el-header>
+
+    <el-container>
+
+    </el-container>
+  </el-container>
+
 </template>
 
-<script>
-import { Component, Vue } from "vue-property-decorator";
-import RatingRecord from "@/types/RatingRecord";
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import RatingRecord from "@/types/RatingRecord";
 
-@Component
-export default class Ratings extends Vue {
-    ratings: RatingRecord[]
-}
+    @Component
+    export default class Ratings extends Vue {
+        ratings: RatingRecord[]
+    }
 </script>
 
 <style scoped></style>
