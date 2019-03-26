83e5452929b66749903c1f4148dd7f6047a35f82
dmitriyace <chistohodovda@gmail.com>
2019-03-27 01:24:25 +0300

    fe-vue: user: add initial user component

---
+++ fe-vue/src/views/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+    import {Component, Vue} from 'vue-property-decorator'
+
+    @Component
+    export default class User extends Vue {
+
+    }
+    Vue.component("User", User)
+
+</script>
+
+<style scoped>
+
+</style>
