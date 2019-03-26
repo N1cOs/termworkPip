14d4e0ee9416e886dea1d9e6e505ee8589b08f9f
dmitriyace <chistohodovda@gmail.com>
2019-03-27 01:20:51 +0300

    fe-vue: college: add parsing specialities

--- fe-vue/src/views/college/College.vue
+++ fe-vue/src/views/college/College.vue
@@ -9,52 +9,52 @@
         {{ college.description }}
       </el-container>
     </el-container>
-	  <el-container>
-		  <el-header>Specialities</el-header>
-		  <el-input></el-input>
-			<div v-for="speciality in specialities" :key="speciality.id">
-				{{speciality.name}}
-			</div>
-	  </el-container>
+    <el-container>
+      <el-header>Specialities</el-header>
+      <el-input></el-input>
+      <div v-for="speciality in specialities" :key="speciality.id">
+        {{speciality.name}}
+      </div>
+    </el-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import College from "@/types/College";
-import { mockCollege } from "@/mock/Colleges";
-import Speciality from "@/types/Speciality";
-import {mockSpecialities} from "@/mock/Specialities";
+    import {Component, Vue} from "vue-property-decorator";
+    import College from "@/types/College";
+    import {mockCollege} from "@/mock/Colleges";
+    import Speciality from "@/types/Speciality";
+    import {mockSpecialities} from "@/mock/Specialities";
 
-@Component
-export default class Colleges extends Vue {
-  colleges: College[] = mockCollege;
-  college: College = {
-    id: 1,
-    name: "0",
-    description: ""
-  };
+    @Component
+    export default class Colleges extends Vue {
+        colleges: College[] = mockCollege;
+        college: College = {
+            id: 1,
+            name: "0",
+            description: ""
+        };
 
-  specialities: Speciality[] = mockSpecialities
+        specialities: Speciality[] = mockSpecialities
 
-  mounted() {
-    // this.getCollege(this.$route.params.id);
-    this.college = this.colleges.find(
-      college => college.id === parseInt(this.$route.params.id)
-    );
+        mounted() {
+            // this.getCollege(this.$route.params.id);
+            this.college = this.colleges.find(
+                college => college.id === parseInt(this.$route.params.id)
+            );
 
 
-    // console.log(this.$route.params.id);
-  }
+            // console.log(this.$route.params.id);
+        }
 
-  // getCollege(id: string) {
-  //   this.college = this.colleges[parseInt(id) - 1];
-  // }
+        // getCollege(id: string) {
+        //   this.college = this.colleges[parseInt(id) - 1];
+        // }
 
-  // findCollege(id: string){
-  //
-  // }
-}
+        // findCollege(id: string){
+        //
+        // }
+    }
 </script>
 
 <style scoped></style>
